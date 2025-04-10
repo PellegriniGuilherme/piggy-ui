9faecc5a721f2a806b514eb3ecdf346bb639410a
Guilherme Pellegrini <pellegrini458@gmail.com>
2025-04-10 18:42:03 -0300

    fix: update version to 1.0.7 in package.json and refine vite.config.ts configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./package"),
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'package/index.ts'),
+      name: 'PiggyUI',
+      fileName: 'index',
+      formats: ['es']
     },
-  },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
