87fce485463838df38fd92a991cfbbf6ad366a45
Guilherme Pellegrini <pellegrini458@gmail.com>
2025-04-11 09:43:15 -0300

    feat: add vite-plugin-css-injected-by-js and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   plugins: [
@@ -12,7 +13,8 @@ export default defineConfig({
       include: ['package'],
       insertTypesEntry: true,
       copyDtsFiles: true
-    })
+    }),
+    cssInjectedByJsPlugin()
   ],
   build: {
     lib: {
@@ -23,13 +25,16 @@ export default defineConfig({
     },
     outDir: 'dist/package',
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'tailwindcss'],
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss'
         }
       }
-    }
+    },
+    sourcemap: true,
+    emptyOutDir: true
   }
 })
