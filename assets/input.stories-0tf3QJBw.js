e7aa4c4272662f1a5542ea478482b9947e50b102
Guilherme Pellegrini <pellegrini458@gmail.com>
2025-04-15 01:38:13 -0300

    Updates

--- assets/input.stories-0tf3QJBw.js
+++
@@ -1,6 +0,0 @@
-import{I as a}from"./input-wyKKcooF.js";import"./jsx-runtime-D_zvdyIk.js";import"./utils-DjqsqOe8.js";const c={title:"PiggyUI/Components/Input",component:a,tags:["autodocs"]},e={args:{type:"text",placeholder:"Enter text here"}};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    type: 'text',
-    placeholder: 'Enter text here'
-  }
-}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,c as default};
---
+++ assets/separator.stories-DgZUI_tt.js
@@ -0,0 +1,31 @@
+import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{S as t}from"./separator-qtMDfBEG.js";import{c as f}from"./utils-DjqsqOe8.js";import"./index-D4lIrffr.js";import"./index-BYRCaeiI.js";import"./index-BJ-cGVAi.js";import"./index-DsJinFGm.js";const b={title:"PiggyUI/Components/Separator",component:t,tags:["autodocs"],argTypes:{orientation:{options:["horizontal","vertical"],control:{type:"select"}}}},r={render:e=>{const i=e.orientation==="vertical";return a.jsxs("div",{className:f("bg-neutral-100 p-4 w-full",i?"flex items-center space-x-4 h-24":"flex flex-col space-y-2"),children:[a.jsx("span",{children:i?"Esquerda":"Cima"}),a.jsx(t,{...e}),a.jsx("span",{children:i?"Direita":"Baixo"})]})},args:{orientation:"horizontal"}},s={render:e=>a.jsxs("div",{className:"flex flex-col space-y-2 bg-neutral-100 p-4 w-full",children:[a.jsx("span",{children:"Cima"}),a.jsx(t,{...e}),a.jsx("span",{children:"Baixo"})]}),args:{orientation:"horizontal"}},n={render:e=>a.jsxs("div",{className:"flex items-center space-x-4 h-24 bg-neutral-100 p-4",children:[a.jsx("span",{children:"Esquerda"}),a.jsx(t,{...e}),a.jsx("span",{children:"Direita"})]}),args:{orientation:"vertical"}};var o,c,l;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    const isVertical = args.orientation === "vertical";
+    return <div className={cn("bg-neutral-100 p-4 w-full", isVertical ? "flex items-center space-x-4 h-24" : "flex flex-col space-y-2")}>
+        <span>{isVertical ? "Esquerda" : "Cima"}</span>
+        <Separator {...args} />
+        <span>{isVertical ? "Direita" : "Baixo"}</span>
+      </div>;
+  },
+  args: {
+    orientation: "horizontal"
+  }
+}`,...(l=(c=r.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var p,d,m;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => <div className="flex flex-col space-y-2 bg-neutral-100 p-4 w-full">
+      <span>Cima</span>
+      <Separator {...args} />
+      <span>Baixo</span>
+    </div>,
+  args: {
+    orientation: "horizontal"
+  }
+}`,...(m=(d=s.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var x,u,g;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: args => <div className="flex items-center space-x-4 h-24 bg-neutral-100 p-4">
+      <span>Esquerda</span>
+      <Separator {...args} />
+      <span>Direita</span>
+    </div>,
+  args: {
+    orientation: "vertical"
+  }
+}`,...(g=(u=n.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};const D=["Default","Horizontal","Vertical"];export{r as Default,s as Horizontal,n as Vertical,D as __namedExportsOrder,b as default};
