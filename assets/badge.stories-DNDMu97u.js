c2024cc0cf50cbec58dad2e4fcaa24b5ecfb0d92
Guilherme Pellegrini <pellegrini458@gmail.com>
2025-04-16 23:12:16 -0300

    Updates

---
+++ assets/badge.stories-DNDMu97u.js
@@ -0,0 +1,26 @@
+import{j as O}from"./jsx-runtime-D_zvdyIk.js";import{S as j}from"./index-CkDMrSqW.js";import{c as _}from"./index-3UDGtr2f.js";import{c as C}from"./utils-DjqsqOe8.js";import"./index-D4lIrffr.js";const V=_("inline-flex items-center justify-center rounded-md border px-4 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",{variants:{variant:{default:"border-transparent bg-brand-400 text-primary-foreground [a&]:hover:bg-brand-200",secondary:"border-transparent bg-secondary-500 text-white [a&]:hover:bg-secondary-400",destructive:"border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",outline:"text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",success:"border-transparent bg-success-400 text-white [a&]:hover:bg-success-300 focus-visible:ring-success-200 dark:focus-visible:ring-success-300 dark:bg-success-200"}},defaultVariants:{variant:"default"}});function y({className:S,variant:B,asChild:w=!1,...D}){const k=w?j:"span";return O.jsx(k,{"data-slot":"badge",className:C(V({variant:B}),S),...D})}y.__docgenInfo={description:"",methods:[],displayName:"Badge",props:{asChild:{required:!1,tsType:{name:"boolean"},description:"",defaultValue:{value:"false",computed:!1}}}};const P={title:"PiggyUI/Components/Badge",component:y,tags:["autodocs"]},e={args:{variant:"default",children:"Default Badge"}},r={args:{variant:"secondary",children:"Secondary Badge"}},a={args:{variant:"success",children:"Success Badge"}},s={args:{variant:"destructive",children:"Destructive Badge"}},t={args:{variant:"outline",children:"Outline Badge"}};var n,o,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    variant: 'default',
+    children: 'Default Badge'
+  }
+}`,...(c=(o=e.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var i,d,u;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary',
+    children: 'Secondary Badge'
+  }
+}`,...(u=(d=r.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,l,p;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    variant: 'success',
+    children: 'Success Badge'
+  }
+}`,...(p=(l=a.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var v,m,f;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: 'destructive',
+    children: 'Destructive Badge'
+  }
+}`,...(f=(m=s.parameters)==null?void 0:m.docs)==null?void 0:f.source}}};var b,h,x;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    variant: 'outline',
+    children: 'Outline Badge'
+  }
+}`,...(x=(h=t.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};const R=["Default","Secondary","Success","Destructive","Outline"];export{e as Default,s as Destructive,t as Outline,r as Secondary,a as Success,R as __namedExportsOrder,P as default};
